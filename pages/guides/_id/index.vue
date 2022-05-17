025b785758e94cc5794cdad728ff7eee732ea704
TKY0814 <takutakusuzusuzu@gmail.com>
2022-05-17 17:25:16 +0900

    噴き出しを追加しました。依頼費用の価格の部分だけ色を変えました。

--- pages/guides/_id/index.vue
+++ pages/guides/_id/index.vue
@@ -46,7 +46,6 @@
                     </v-col>
                   </v-col>
                 </v-row>
-
                 <v-spacer class="my-16" />
                   <!--活動エリア -->
                   <v-row>
@@ -56,9 +55,7 @@
                   </v-row>
                   <!-- 得意分野 -->
                   <v-row>
-                    
                       <p class="h7  pl-3 mt-3">得意分野</p>
-                    
                     <v-col  v-for="genre in content.genres" :key="genre.id">
                       <p class="text-h7 text-center" >{{ genre }}</p>
                     </v-col>
@@ -87,29 +84,27 @@
               <p class="font-weight-bold">ガイドさんへの質問コーナー</p>
               <p class="text-h7  mb-12">Q1.一番登っている山はどこですか？</p>
               <v-row>
-                <v-img class="ml-4"  contain max-height="40" max-width="40" :src="content.image.url" />
+                <v-img class="ml-4 mt-7"  contain max-height="40" max-width="40" :src="content.image.url" />
               <v-col cols="10">
-                <p class="text-h7  mb-12 ">最も好きな山は妙義山です。 生まれて初めて岩場に挑戦した山で、最も登っている山でもあります。 頂上からの景色がとても素敵です。 ツアーに参加されたお客様からの評価も高く、お勧めの一山です。 是非一緒に挑戦しましょう。</p>
+                <div class="my-parts-content">最も好きな山は妙義山です。 生まれて初めて岩場に挑戦した山で、最も登っている山でもあります。 頂上からの景色がとても素敵です。 ツアーに参加されたお客様からの評価も高く、お勧めの一山です。 是非一緒に挑戦しましょう。
+                </div>
               </v-col>
               </v-row>
               <!--TODO:コンテンツの埋め込み-->
               <v-divider></v-divider>
             </v-col>
             <!-- アピールメッセージ -->
-            
               <v-col>
                 <p class="font-weight-bold">アピールメッセージ</p>
                 <p class="text-h7  mb-12">得意なことは、食べられる野草を使って調理をしたり、スペシャルドリンクを考えたりする ことです。 野草料理を召し上がった人に、美味しいと思ってもらえるよう、いつも工夫とリサーチをし ています。 地域に伝わる野草の言い伝えも紹介しています。料理を交えてお話しができれば幸いです。</p>
                 <!--TODO:コンテンツの埋め込み-->
               </v-col>
-            
           </v-col>
-
           <!-- 予約機能部分 -->
           <v-col cols="4">
             
             <v-sheet rounded="xl" class="sticky" elevation="5" color="#FFFFFF">
-              <p class="text-h5 ml-11 pt-8">依頼費用の目安￥5,300から</p>
+              <p class="request-cost text-h5 ml-11 pt-8">依頼費用の目安<span>￥5,300</span>から</p>
               <!--TODO:コンテンツの埋め込み-->
               <div >
                 <v-list rounded="xl" color="##FFFFFF">
@@ -135,9 +130,6 @@
                       </v-list-item-title>
                     </v-list-item-content>
                   </v-list-item>
-
-                
-
                   <v-list-item
                     link
                     color="#000000"
@@ -183,6 +175,53 @@ export default {
 width:100%;
 height:100%;
 object-fit:cover;
+}
+
+
+.request-cost span {
+  color:#D13A3A;
+}
+
+.my-parts-content {
+  position: relative;
+  display: inline-block;
+  margin: 0 0 1.5em 15px;
+  padding: 9px 12px;
+  min-width: 120px;
+  max-width: 100%;
+  color: #555;
+  font-size: 16px;
+  background: #FFF;
+  border: solid 1px #555;
+  border-radius: 8px;
+  box-sizing: border-box;
+}
+
+.my-parts-content::before {
+  content: "";
+  position: absolute;
+  top: 30%;
+  left: -23px;
+  margin-top: -12px;
+  border: 12px solid transparent;
+  border-right: 11px solid #FFF;
+  z-index: 2;
+}
+
+.my-parts-content::after {
+  content: "";
+  position: absolute;
+  top: 30%;
+  left: -29px;
+  margin-top: -14px;
+  border: 14px solid transparent;
+  border-right: 15px solid #555;
+  z-index: 1;
+}
+
+.my-parts-content p {
+  margin: 0;
+  padding: 0;
 }
 
 .overflow-hidden{
