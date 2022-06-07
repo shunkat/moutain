fe8ec41461b703eb2f18a40bd25b03f1ca33d033
shunkat <shunkat@icloud.com>
2022-06-07 23:22:08 +0900

    出社のため一旦保存

---
+++ components/GuidesList.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="(content,index) in sections.contents" :key=index>
+            <v-col
+              
+              class="mt-2"
+              cols="12"
+            >
+              <strong>{{ section.title }}</strong>
+            </v-col>
+
+            <v-col
+              v-for="guide in guides.contents"
+              :key="guide.id"
+            >
+                <nuxt-link :to="`/guides/${guide.id}/`">
+                    <v-card elevation="2" height="200">
+                        <v-card-title>{{ guide.name }}</v-card-title>
+                        <v-img :src="guide.image.url" height="240" />
+                    </v-card>
+                </nuxt-link>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+</template>
+
+<script>
+export default {
+    async asyncData({ $microcms }) {
+        const guides = await $microcms.get({
+            endpoint: "guides",
+        });
+        const sections = await $microcms.get({
+            endpoint: "sections",
+        });
+        return {
+            guides,
+            sections,
+        };  
+    },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,29 @@
 <template>
   <section class="home">
     <HomeHero></HomeHero>
+    <GuidesList></GuidesList>
   </section>
 </template>
 <script>
 import HomeHero from "../components/HomeHero.vue";
+import GuidesList from "../components/GuidesList.vue";
 
 export default {
   components: {
     HomeHero,
+    GuidesList,
+  },
+  async asyncData({ $microcms }) {
+    const guides = await $microcms.get({
+      endpoint: "guides",
+    });
+    const sections = await $microcms.get({
+      endpoint: "sections",
+    });
+    return {
+      guides,
+      sections,
+    };
   },
 };
 </script>
